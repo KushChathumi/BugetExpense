8d571db261611db786825f5606ec3e5d5d2da1fb
Kushani Abeysinghe <kushani96abeysinghe@gmail.com>
2023-09-02 09:38:05 +0530

    Initial Commit

---
+++ BugetExpense/BugetExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugetExpenseApp.swift
+//  BugetExpense
+//
+//  Created by Kushani Abeysinghe on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct BugetExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
