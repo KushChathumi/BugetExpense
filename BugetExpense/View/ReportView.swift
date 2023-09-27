705ca8a733395751692ff8261340b79b2aaee8be
Kushani Abeysinghe <kushani96abeysinghe@gmail.com>
2023-09-27 22:30:14 +0530

    report view created

--- BugetExpense/View/ReportView.swift
+++ BugetExpense/View/ReportView.swift
@@ -6,19 +6,50 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ReportView: View {
+    
+    @ObservedObject var viewModel: expsense
+    @ObservedObject var loginVM: LoginViewModel
+    let userID: String? // Assuming you have the user's ID
+
+    
     var body: some View {
         NavigationStack  {
             List{
+                VStack{
+                    Text("title")
+                    
+//                    ForEach(viewModel.groupedExpenses.keys.sorted(by: >), id: \.self) { date in
+//                        Section(header: Text(dateFormatter.string(from: date)).foregroundColor(Color("purple2"))) {
+//                            ForEach(viewModel.groupedExpenses[date]!, id: \.self) { expense in
+//                                Text(expense.title)
+//                                    .fontWeight(.semibold)
+//                                    .font(.title3)
+//                                    .foregroundColor(Color("purple2"))
+//                            }}}
+                }
                 
-            }.navigationTitle("Report")
-        }
+                
+                                Chart {
+                                    ForEach(viewModel.expenses){ data in
+                                        BarMark(x: PlottableValue.value("Type", data.title),
+                                                y: PlottableValue.value("amount", data.amount))
+                                    }
+                                }
+            }
+        }.navigationTitle("Report")
+            .onAppear {
+                if let userID = userID {
+                    self.viewModel.fetchExpenses(forUserID: userID)
+                }
+            }
+    }
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
     }
 }
 
-struct ReportView_Previews: PreviewProvider {
-    static var previews: some View {
-        ReportView()
-    }
-}
---
+++ BugetExpense/ViewModel/ReportViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ReportViewModel.swift
+//  BugetExpense
+//
+//  Created by Kushani Abeysinghe on 2023-09-27.
+//
+
+import Foundation
+
+struct ReportItem: Identifiable {
+    let id = UUID()
+    let date: Date
+    let title: String
+    let subtitle: String
+    let amount: Double
+}
+
+class ReportViewModel: ObservableObject {
+    @Published var reportItems: [ReportItem] = []
+    
+    init(expensesViewModel: expsense) {
+        // Listen for changes in the expensesViewModel's expenses property
+        expensesViewModel.$expenses
+            .map { expenses in
+                // Transform the expenses data into ReportItems
+                return expenses.map { expense in
+                    ReportItem(date: expense.date, title: expense.title, subtitle: expense.subtitle, amount: expense.amount)
+                }
+            }
+            .assign(to: &$reportItems)
+        print("Report Items: \(reportItems)")
+    }
+}
